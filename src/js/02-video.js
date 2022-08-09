9a5f7278faea10fdb86f6c346e31be496493fcee
AndriyKozh <max190716@ukr.net>
2022-08-10 00:05:00 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,17 @@
-// const player = new Player('vimeo-player');
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
 const player = new Player('vimeo-player');
-
-const CURRENT_TIME = 'videoplayer-current-time';
-
+const TIME_VIDEO = 'videoplayer-current-time';
 player.on(
   'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
-    console.log('timer work');
+  throttle(function (ev) {
+    localStorage.setItem(TIME_VIDEO, JSON.stringify(ev.seconds));
   }, 1000)
 );
 
-if (localStorage.getItem(CURRENT_TIME)) {
-  player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
+if (localStorage.getItem(TIME_VIDEO)) {
+  player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_VIDEO)));
 }
