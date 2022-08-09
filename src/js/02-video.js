766ca45b965ffc4fbc8dadaea91cca1629c60676
AndriyKozh <max190716@ukr.net>
2022-08-09 18:26:30 +0300

    errGitHub

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
-import Player from '@vimeo/player';
-console.log(Player);
+// import Player from '@vimeo/player';
+// console.log(Player);
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-console.log(player);
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+// console.log(player);
 
-let keceivedTime;
+// let keceivedTime;
 
-player.on('play', onPlay);
+// player.on('play', onPlay);
 
-const onPlay = function (data) {
-  //   data.preventDefault();
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-  keceivedTime = localStorage.getItem('videoplayer-current-time');
-};
-console.log(keceivedTime);
+// const onPlay = function (data) {
+//   //   data.preventDefault();
+//   localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+//   keceivedTime = localStorage.getItem('videoplayer-current-time');
+// };
+// console.log(keceivedTime);
